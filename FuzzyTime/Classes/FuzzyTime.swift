13e7988eaaecef918402231929d0889c709792a1
Kyle <kylemilner@me.com>
2016-11-01 21:44:48 +0000

    replace bootstrapped class with initial FuzzyTime implementation

---
+++ FuzzyTime/Classes/FuzzyTime.swift
@@ -0,0 +1,21 @@
+//
+//  FuzzyTime.swift
+//  Pods
+//
+//  Created by Kyle Milner on 01/11/2016.
+//
+//
+
+import Foundation
+
+class FuzzyTime {
+
+    init() {
+
+    }
+
+    func fuzz(date: NSDate) -> String {
+        return "Sometime"
+    }
+
+}
