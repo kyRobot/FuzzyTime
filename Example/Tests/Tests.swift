7ebee1c1d27490c358651cb635f238b91353c185
Kyle <kylemilner@me.com>
2016-11-01 22:40:09 +0000

    unit test for basic fuzzing

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,29 +1,29 @@
 import UIKit
 import XCTest
-import FuzzyTime
+@testable import FuzzyTime
+import Foundation
 
 class Tests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testFuzzGivenDate() {
+        // Given
+        let now = NSDate()
+
+        // When
+        let fuzzy = FuzzyTime().fuzz(date: now)
+
+        // Then
+        XCTAssertTrue(fuzzy == "Sometime")
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+
 }
